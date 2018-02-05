252e0d39b2229b46f289cd59d41b3c440bd99d91
Karlas <karolis.zagarnauskas@gmail.com>
2018-02-05 14:17:42 +0200

    1st comm

---
+++ src/com/company/Main.java
@@ -0,0 +1,274 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main objektas = new Main();
+        int b = 0;
+        String eilute =" dalinuosi is 10";
+        objektas.metodas1();
+        objektas.metodas2(b++);
+        objektas.metodas3();
+        objektas.metodas4(b++);
+        objektas.metodas5();
+        objektas.metodas6(b++);
+        objektas.metodas7();
+        objektas.metodas8(b++);
+        objektas.metodas9();
+        objektas.metodas10(b++,eilute);
+        objektas.metodas11();
+        objektas.metodas12(b++);
+        objektas.metodas13();
+        objektas.metodas14(b++);
+        objektas.metodas15();
+        objektas.metodas16(b++);
+        objektas.metodas17();
+        objektas.metodas18(b++);
+        objektas.metodas19();
+        objektas.metodas20(b++, eilute);
+        objektas.metodas21();
+        objektas.metodas22(b++);
+        objektas.metodas23();
+        objektas.metodas24(b++);
+        objektas.metodas25();
+        objektas.metodas26(b++);
+        objektas.metodas27();
+        objektas.metodas28(b++);
+        objektas.metodas29();
+        objektas.metodas30(b++, eilute);
+        objektas.metodas31();
+        objektas.metodas32(b++);
+        objektas.metodas33();
+        objektas.metodas34(b++);
+        objektas.metodas35();
+        objektas.metodas36(b++);
+        objektas.metodas37();
+        objektas.metodas38(b++);
+        objektas.metodas39();
+        objektas.metodas40(b++, eilute);
+        objektas.metodas41();
+        objektas.metodas42(b++);
+        objektas.metodas43();
+        objektas.metodas44(b++);
+        objektas.metodas45();
+        objektas.metodas46(b++);
+        objektas.metodas47();
+        objektas.metodas48(b++);
+        objektas.metodas49();
+        objektas.metodas50(b++,eilute);
+        objektas.metodas51();
+        objektas.metodas52(b++);
+        objektas.metodas53();
+        objektas.metodas54(b++);
+        objektas.metodas55();
+        objektas.metodas56(b++);
+        objektas.metodas57();
+        objektas.metodas58(b++);
+        objektas.metodas59();
+        objektas.metodas60(b++, eilute);
+
+    }
+
+
+    public void metodas1() {
+        System.out.println("metodas1 ");
+
+    }
+
+    public void metodas2(int a) {
+        System.out.println("metodas2 " + a);
+
+
+    }
+
+    public void metodas3() {
+        System.out.println("metodas3 ");
+
+
+    }
+
+    public void metodas4(int a) {
+        System.out.println("metodas4 " + a);
+
+    }
+
+    public void metodas5() {
+        System.out.println("metodas5 ");
+    }
+
+    public void metodas6(int a) {
+        System.out.println("metodas6 " + a);
+    }
+
+    public void metodas7() {
+        System.out.println("metodas7 ");
+
+    }
+
+    public void metodas8(int a) {
+        System.out.println("metodas8 " + a);
+    }
+
+    public void metodas9() {
+        System.out.println("metodas9 ");
+    }
+
+    public void metodas10(int a, String eilute) {
+        System.out.println("metodas10 " + a + eilute);
+    }
+
+    public void metodas11() {
+        System.out.println("metodas11 ");
+    }
+
+    public void metodas12(int a) {
+        System.out.println("metodas12 " + a);
+    }
+
+    public void metodas13() {
+        System.out.println("metodas13 ");
+    }
+    public void metodas14(int a) {
+        System.out.println("metodas14 " +a);
+    }
+    public void metodas15() {
+        System.out.println("metodas15 ");
+    }
+    public void metodas16(int a) {
+        System.out.println("metodas16 "+ a);
+    }
+    public void metodas17() {
+        System.out.println("metodas17 ");
+    }
+    public void metodas18 (int a){
+        System.out.println("metodas18 " +a);
+    }
+    public void metodas19() {
+        System.out.println("metodas19 ");
+    }
+    public void metodas20(int a,String eilute){
+        System.out.println("metodas 20 " +a+eilute);
+    }
+    public void metodas21(){
+        System.out.println("metodas21 ");
+    }
+    public void metodas22(int a){
+        System.out.println("metodas22 " +a);
+    }
+    public void metodas23 (){
+        System.out.println("metodas23 ");
+    }
+    public void metodas24 (int a){
+        System.out.println("metodas24 " +a);
+    }
+    public void metodas25(){
+        System.out.println("metodas25 ");
+    }
+    public void metodas26(int a){
+        System.out.println("metodas26 " +a);
+    }
+    public void metodas27(){
+        System.out.println("metodas27 ");
+    }
+    public void metodas28(int a){
+        System.out.println("metodas 28 " +a);
+    }
+    public void metodas29(){
+        System.out.println("metodas29 ");
+    }
+    public void metodas30(int a, String eilute){
+        System.out.println("metodas30 " +a+ eilute);
+    }
+    public void metodas31(){
+        System.out.println("metodas31 ");
+    }
+    public void metodas32(int a){
+        System.out.println("metodas32 " +a);
+    }
+    public void metodas33(){
+        System.out.println("metodas33 ");
+    }
+    public void metodas34(int a){
+        System.out.println("metodas34 " +a);
+    }
+    public void metodas35(){
+        System.out.println("metodas35 ");
+    }
+    public void metodas36(int a){
+        System.out.println("metodas36 " +a);
+    }
+    public void metodas37(){
+        System.out.println("metodas37 ");
+    }
+    public void metodas38(int a){
+        System.out.println("metodas38 " +a);
+    }
+    public void metodas39(){
+        System.out.println("metodas39 ");
+    }
+    public void metodas40(int a,String eilute){
+        System.out.println("metodas40 " +a+eilute);
+    }
+    public void metodas41(){
+        System.out.println("metodas41 ");
+    }
+    public void metodas42(int a){
+        System.out.println("metodas42 " +a);
+    }
+    public void metodas43(){
+        System.out.println("metodas43 ");
+    }
+    public void metodas44(int a){
+        System.out.println("metodas44 "+ a);
+    }
+    public void metodas45(){
+        System.out.println("metodas45 ");
+    }
+    public void metodas46(int a){
+        System.out.println("metodas46 "+ a);
+    }
+    public void metodas47(){
+        System.out.println("metodas47 ");
+    }
+    public void metodas48(int a){
+        System.out.println("metodas 48 "+ a);
+    }
+    public void metodas49(){
+        System.out.println("metodas49 ");
+    }
+    public void metodas50(int a, String eilute){
+        System.out.println("metodas50 " +a+ eilute);
+    }
+    public void metodas51(){
+        System.out.println("metodas51 ");
+    }
+    public void metodas52(int a){
+        System.out.println("metodas52 "+ a);
+    }
+    public void metodas53(){
+        System.out.println("metodas53 ");
+    }
+    public void metodas54(int a){
+        System.out.println("metodas54 "+ a);
+    }
+    public void metodas55(){
+        System.out.println("metodas55");
+    }
+    public void metodas56(int a){
+        System.out.println("metodas56 "+ a);
+    }
+    public void metodas57(){
+        System.out.println("metodas57 ");
+    }
+    public void metodas58(int a){
+        System.out.println("metodas58 "+ a);
+    }
+    public void metodas59(){
+        System.out.println("metodas59 ");
+    }
+    public void metodas60(int a, String eilute){
+        System.out.println("metodas60 "+ a+eilute);
+    }
+}
